b6bd591a6bc2b8560fe1d7ac33ae9d441ebbd2f9
tikasan <satak47cpc@gmail.com>
2017-01-25 22:46:44 +0900

    fix:トップコントローラーを正しいデータ呼び出しに変更

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -9,19 +9,13 @@ Class HomeController extends Controller
     {
         $this->data['title'] = 'Candy Clip';
 
-		// ページングのリンク
-		$page = (int)Input::get('page');
-
 		// レシピテーブル（ORM）
 		$Recipe = new Recipe();
 		$Tag = new Tag();
 		try {
 			$this->data['categories'] = $Tag->getCategory();
 			$this->data['scenes'] = $Tag->getScene();
-			$findRecipes = $Recipe->newQuery()->orderBy('created_at', 'desc')->get();
-			$this->data['recipes'] = $Recipe->findByQueryPerPage($findRecipes, $page);
-			$this->data['pager'] = $Recipe->paginationNav((int)$page, $this->siteUrl('recipe'))
-				->get_html(PAGING_THEMES_PATH);
+			$this->data['recipes'] = $findRecipes = $Recipe->all();
 		} catch (\SQLiteException $e) {
 			App::flash('messageError', "データベースエラーが発生しました。管理者にお問い合わせください。");
 			Response::redirect($this->siteUrl('report'));
