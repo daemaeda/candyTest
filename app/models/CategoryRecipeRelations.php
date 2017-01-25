34be060042d8ede75b2570af4eb88fbb5f26a5d4
tikasan <satak47cpc@gmail.com>
2017-01-25 14:08:36 +0900

    .

--- app/models/CategoryRecipeRelations.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Candy\Base\Model;
-
-class CategoryRecipeRelations extends Model {
-
-    protected $table = 'category_recipe_relations';
-    protected $fillable = ['recipe_id', 'category_s_id', 'category_m_id', 'category_l_id'];
-}
