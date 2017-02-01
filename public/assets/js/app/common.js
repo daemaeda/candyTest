3f49004b51e32a14d76cfe2de2c28d1fc65371a9
前田咲満 <ema@maedasakimannoMacBook-Air.local>
2017-02-01 14:27:48 +0900

    あっど：いろいろついかしたで

---
+++ public/assets/js/app/common.js
@@ -0,0 +1,15 @@
+$(function() {
+	var $header = $('header');
+	// Nav Fixed
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 350) {
+			$header.addClass('fixed');
+		} else {
+			$header.removeClass('fixed');
+		}
+	});
+	// Nav Toggle Button
+	$('#nav-toggle').click(function(){
+		$header.toggleClass('open');
+	});
+});
